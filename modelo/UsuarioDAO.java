0036ab50005cf27bad42607ab9ec4086d48e80c9
landerdam-lab <landerdam@gmail.com>
2026-02-09 09:04:26 +0100

    1

--- modelo/UsuarioDAO.java
+++ modelo/UsuarioDAO.java
@@ -8,29 +8,5 @@ import java.sql.Statement;
 
 public class UsuarioDAO {
 
-	Connection conn = null;
-	Statement stmt = null;
-	String DB_URL = "jdbc:oracle:thin:@192.168.0.72:1521:ORCLCDB";
-	String USER = "tienda_empleado_dos";
-	String PASS = "Almi12345";
-
-	public UsuarioDAO () {
-		
-		try {
-			conn = DriverManager.getConnection(DB_URL, USER, PASS);
-
-			if (conn != null) {
-				System.out.println("Connected to the Oracle DB!");
-				stmt = conn.createStatement();
-			} else {
-				System.out.println("Failed to make connection!");
-			}
-
-		} catch (SQLException e) {
-			System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
 	
 }
